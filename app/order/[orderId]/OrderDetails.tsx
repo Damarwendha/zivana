6cbc40b5aabeb573ab3b84d52bca449a1a3641a7
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2024-06-25 19:58:14 +0700

    fix(order detail): fix total price

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -18,85 +18,87 @@ moment.locale("id");
 const OrderDetails: React.FC<OrderDetailsProps> = ({ order }) => {
   console.log("or", order);
   return (
-      <div className="max-w-[1150px] m-auto flex flex-col gap-2">
-          <div className="mt-8">
-              <Heading title="Detail Order"/>
-          </div>
-          {/* <div>Order ID: {order.id}</div> */}
-          <div>
-              Total Jumlah:{" "}
-              <span className="font-bold">{formatPrice(order.amount / 100)}</span>
-          </div>
-          <div>
-              Biaya ongkos kirim:{" "}
-              <span className="font-bold">{formatPrice(order.ongkir)}</span>
-          </div>
-          <div className="flex gap-2 items-center">
-              <div>Status pembayaran:</div>
-              <div>
-                  {order.status === "pending" ? (
-                      <Status
-                          text="Belum Membayar"
-                          icon={MdAccessTimeFilled}
-                          bg="bg-slate-200"
-                          color="text-slate-700"
-                      />
-                  ) : order.status === "complete" ? (
-                      <Status
-                          text="Selesai Pembayaran"
-                          icon={MdDone}
-                          bg="bg-green-200"
-                          color="text-green-700"
-                      />
-                  ) : (
-                      <></>
-                  )}
-              </div>
-          </div>
-          <div className="flex gap-2 items-center">
-              <div>Status pengiriman:</div>
-              <div>
-                  {order.deliveryStatus === "pending" ? (
-                      <Status
-                          text="Belum Dikirim"
-                          icon={MdAccessTimeFilled}
-                          bg="bg-slate-200"
-                          color="text-slate-700"
-                      />
-                  ) : order.deliveryStatus === "dispatched" ? (
-                      <Status
-                          text="Dalam Perjalanan"
-                          icon={MdDeliveryDining}
-                          bg="bg-purple-200"
-                          color="text-purple-700"
-                      />
-                  ) : order.deliveryStatus === "delivered" ? (
-                      <Status
-                          text="Sampai Tujuan"
-                          icon={MdDone}
-                          bg="bg-green-200"
-                          color="text-green-700"
-                      />
-                  ) : (
-                      <></>
-                  )}
-              </div>
-          </div>
-          <div>Waktu: {moment(order.createDate).fromNow()}</div>
-          <div>
-              <h2 className="font-semibold mt-4 mb-2">Produk dipesan</h2>
-              <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center">
-                  <div className="col-span-2 justify-self-start">PRODUK</div>
-                  <div className="justify-self-center">HARGA</div>
-                  <div className="justify-self-center">JUMLAH</div>
-                  <div className="justify-self-end">TOTAL</div>
-              </div>
-              {order.products &&
-                  order.products.map((item) => {
-                      return <OrderItem key={item.id} item={item}></OrderItem>;
-                  })}
-          </div>
+    <div className="max-w-[1150px] m-auto flex flex-col gap-2">
+      <div className="mt-8">
+        <Heading title="Detail Order" />
       </div>
+      {/* <div>Order ID: {order.id}</div> */}
+      <div>
+        Total Jumlah:{" "}
+        <span className="font-bold">
+          {formatPrice(order.amount + order.ongkir)}
+        </span>
+      </div>
+      <div>
+        Biaya ongkos kirim:{" "}
+        <span className="font-bold">{formatPrice(order.ongkir)}</span>
+      </div>
+      <div className="flex gap-2 items-center">
+        <div>Status pembayaran:</div>
+        <div>
+          {order.status === "pending" ? (
+            <Status
+              text="Belum Membayar"
+              icon={MdAccessTimeFilled}
+              bg="bg-slate-200"
+              color="text-slate-700"
+            />
+          ) : order.status === "complete" ? (
+            <Status
+              text="Selesai Pembayaran"
+              icon={MdDone}
+              bg="bg-green-200"
+              color="text-green-700"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <div>Status pengiriman:</div>
+        <div>
+          {order.deliveryStatus === "pending" ? (
+            <Status
+              text="Belum Dikirim"
+              icon={MdAccessTimeFilled}
+              bg="bg-slate-200"
+              color="text-slate-700"
+            />
+          ) : order.deliveryStatus === "dispatched" ? (
+            <Status
+              text="Dalam Perjalanan"
+              icon={MdDeliveryDining}
+              bg="bg-purple-200"
+              color="text-purple-700"
+            />
+          ) : order.deliveryStatus === "delivered" ? (
+            <Status
+              text="Sampai Tujuan"
+              icon={MdDone}
+              bg="bg-green-200"
+              color="text-green-700"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+      <div>Waktu: {moment(order.createDate).fromNow()}</div>
+      <div>
+        <h2 className="font-semibold mt-4 mb-2">Produk dipesan</h2>
+        <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center">
+          <div className="col-span-2 justify-self-start">PRODUK</div>
+          <div className="justify-self-center">HARGA</div>
+          <div className="justify-self-center">JUMLAH</div>
+          <div className="justify-self-end">TOTAL</div>
+        </div>
+        {order.products &&
+          order.products.map((item) => {
+            return <OrderItem key={item.id} item={item}></OrderItem>;
+          })}
+      </div>
+    </div>
   );
 };
 
