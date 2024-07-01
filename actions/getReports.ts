8a0773c917bbce30ed515d988add3105ec78ea2e
Muhammad Ofie <ofieisranta@gmail.com>
2024-07-02 06:38:09 +0700

    fix: getReports

--- actions/getReports.ts
+++ actions/getReports.ts
@@ -3,21 +3,10 @@ import prisma from '@/libs/prismadb'
 export default async function getReports() {
     try {
         return await prisma.report.findMany({
-            select: {
-                id: true,
-                message: true,
-                createdAt: true,
+            include: {
                 user: {
-                    select: {
-                        id: true,
-                        email: true,
-                        image: true,
-                        name: true,
-                        accounts: {
-                            select: {
-                                type: true,
-                            },
-                        },
+                    include: {
+                        accounts: {},
                     },
                 },
             }
